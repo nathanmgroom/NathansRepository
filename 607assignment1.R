e95348d71caf0e405e985051c29cd275876deea8
Nathan Groom <nathanmgroom@gmail.com>
2014-08-30 11:14:00 -0400

    607 assignment 1
    Nathan Groom

---
+++ 607assignment1.R
@@ -0,0 +1,5 @@
+#Nathan Groom IS 607 assignment 1
+#version of R is: R version 3.0.1 (2013-05-16) -- "Good Sport"
+#version of RStudio is: Version 0.98.953
+#version of PostGreSQL is 9.3.5 on Mac OSX
+#in dataset "sales" from the DMwR package, I see 401145 obs. of 5 variables
